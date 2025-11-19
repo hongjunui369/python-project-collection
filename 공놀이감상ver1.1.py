eac840473c5c23b1aeb4e7d42195893aae8c9c4e
HongJunui <hongjunui369@gmail.com>
2025-11-19 10:39:47 +0900

    ver 1.1 추가기능구현
    스페이스 클릭:정지 / 좌클릭 : 공 1개 추가

---
+++ 공놀이감상ver1.1.py
@@ -0,0 +1,69 @@
+from tkinter import *
+import time, random
+
+
+class Ball:
+    def __init__(self, canvas, color, size, x, y, xspeed, yspeed):
+        self.canvas = canvas
+        self.color = color
+        self.size = size
+        self.x = x
+        self.y = y
+        self.xspeed = xspeed
+        self.yspeed = yspeed
+        self.id = canvas.create_oval(x, y, x + size, y + size, fill=color)
+
+    def move(self):
+        self.canvas.move(self.id, self.xspeed, self.yspeed)
+        (x1, y1, x2, y2) = self.canvas.coords(self.id)
+        (self.x, self.y) = (x1, y1)
+        if x1 <= 0 or x2 >= WIDTH:  # 공의 x좌표 음수이거나 x좌표가 오른쪽 경계를 넘으면
+            self.xspeed = - self.xspeed  # 속도의 부호 반전시킨다.
+        if y1 <= 0 or y2 >= HEIGHT:  # 공의 y좌표 음수이거나 오른쪽 경계를 넘으면
+            self.yspeed = - self.yspeed
+
+
+def add_random_ball(event=None):
+    color = random.choice(color_list)
+    size = random.randrange(10, 100)
+    xspeed = random.randrange(1, 10)
+    yspeed = random.randrange(1, 10)
+    x = random.randrange(0, WIDTH - size)
+    y = random.randrange(0, HEIGHT - size)
+
+    new_ball = Ball(canvas, color, size, x, y, xspeed, yspeed)
+    balls_list.append(new_ball)
+    print(f"새 공 추가! 현재 공 개수: {len(balls_list)}")
+
+
+WIDTH = 800
+HEIGHT = 400
+color_list = ["yellow", "green", "blue", "red", "orange", "pink", "grey", "black"]
+balls_list = []
+window = Tk()
+canvas = Canvas(window, width=WIDTH, height=HEIGHT)
+canvas.pack()
+
+paused = False
+def stop(event):
+    global paused
+    paused = not paused
+
+window.bind("<space>", stop) #스패이스 클릭
+canvas.bind("<Button-1>", add_random_ball)  # 캔버스에서 좌클릭
+
+#초기값
+for i in range(5):
+    color = random.choice(color_list)
+    size = random.randrange(10, 100)
+    xspeed = random.randrange(1, 10)
+    yspeed = random.randrange(1, 10)
+    balls_list.append(Ball(canvas, color, size, 0, 0, xspeed, yspeed))
+
+while True:
+    if not paused:
+     for ball in balls_list:
+        ball.move()
+    window.update()
+    time.sleep(0.03)
+
